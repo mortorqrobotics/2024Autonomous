9db38cb71fc5f3909e4833c5f95b4a510ad0fe90
Daniel Salkin <daniel@walden.vc>
2024-01-07 23:38:13 -0800

    finished writing approximating spaced points

--- src/main/java/org/team1515/Autonomous/utils/bezierUtil.java
+++ src/main/java/org/team1515/Autonomous/utils/bezierUtil.java
@@ -58,5 +58,107 @@ public class bezierUtil {
             return n * factorial(n - 1);
     }
 
+    public static CartesianPoint[] placePoints(ArrayList<Equation> bezierEquation, int points){
+        CartesianPoint[] placedPoints = new CartesianPoint[points];
+        //loops from 0 to 1 incrementing by 1 over the number of sections(points-1)
+        //for each section, calculates the point at said section and adds it to the new arraylist
+        for(int i = 0; i <= 1; i+=(1.0/(points-1))){
+            double x=0.0;
+            double y=0.0;
+            //calculates the values of x and y
+            for(Equation e: bezierEquation){
+                x+=e.applyX(i);
+                y+=e.applyY(i);
+            }
+            placedPoints[i] = new CartesianPoint(x, y);
+        }
+        return placedPoints;
+    }
 
-}
+    public static double getLength(CartesianPoint start, CartesianPoint end){
+        return Math.sqrt(Math.pow((end.getX()-start.getX()), 2)+Math.pow((end.getY()-start.getY()), 2));
+    }
+
+    public static double bezierLength(CartesianPoint[] points){
+        double total = 0.0;
+        for(int i=0; i<points.length-1;i++){
+            total+=getLength(points[i], points[i+1]);
+        }
+        return total;
+    }
+
+    public static CartesianPoint applyBezierEquation(ArrayList<Equation> bezierEquation, double t){
+        double i = 0.0;
+        double j = 0.0;
+        for(Equation p : bezierEquation){
+            i+=p.applyX(t);
+            j+=p.applyY(t); 
+        }
+        return new CartesianPoint(i, j);
+    }
+
+    public static ArrayList<CartesianPoint> spacedPoints(ArrayList<Equation> bezierEquation){
+        //return value
+        ArrayList<CartesianPoint> spacedPoints = new ArrayList<CartesianPoint>();
+        //number of points on our bezier apporximation
+        int n = 100;
+        //set of points on our bezier
+        CartesianPoint[] points = placePoints(bezierEquation, n-2);
+        //use helper functions to calculate average leg length
+        double avgLegLength = bezierLength(points)/(n-1);
+        //set how much we nudge each point every round to(honetsly idk)
+        double stepSize = (1/n)/10;
+        //create an array of t values
+        double[] tValues = new double[n];
+        for(int i = 0;i<n;i++){
+            tValues[i] = (double)i/n;
+        }
+        //4 rounds of nudging
+        for(int i = 0; i<4; i++){
+            //list of lengths of each bezier section
+            double[] legLengths = new double[n-1];
+            for(int j = 0; j<n-1;j++){
+                //note: leg[0] is the leg from point[0] to point[1]
+                legLengths[j] = getLength(points[j], points[j+1]);
+            }
+            //difference between real leg length and average leg length
+            double[]legError = new double[legLengths.length];
+            for(int j = 0; j<legError.length;j++){
+                legError[j] = legLengths[j]-avgLegLength;
+            }
+            //loops thrugh each t(not including first and last) value and nudges it left or right based on legError
+            for(int j = 1; j<tValues.length-1;j++){
+                //moves t in the opposite direction of the error by the step size times the error
+                tValues[j] += (stepSize*legError[j-1]*-1.0);
+                //sets the point to be at that t value
+                points[j] = applyBezierEquation(bezierEquation, tValues[j]);
+            }
+        }
+
+
+//         for (let r=0; r<4; r++) {
+//     let d = [];
+//     for (let j=0; j<m[i]-1; j++) {
+//       d[j] = mag(
+//         (s[i][j+1].x-s[i][j].x),
+//         (s[i][j+1].y-s[i][j].y)
+//       );
+//     }
+//     const d_err = d.map((segment) => {
+//       return (segment - d_avg);
+//     });
+//     let offset = 0;
+//     const cutoff = (i === bezier.length - 1) ? 0 : 1;
+//     for (let j=1; j<m[i]-cutoff; j++) {
+//       offset += d_err[j-1];
+//       t[j] -= step_size * offset;
+//       s[i][j] = Y[i](t[j]);
+//     }
+//   }
+
+
+
+        return spacedPoints;
+    }
+
+}   

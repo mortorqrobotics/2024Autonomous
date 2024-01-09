8690029a4662e8e3b38f88907c51f1bb07f6af91
Hayley <hayley.chechel@gmail.com>
2024-01-09 09:36:23 -0800

    changes to rotateAngle

--- src/main/java/org/team1515/Autonomous/Commands/RotateAngle.java
+++ src/main/java/org/team1515/Autonomous/Commands/RotateAngle.java
@@ -11,22 +11,32 @@ import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 
-public class RotateToAngle extends CommandBase {
+public class RotateAngle extends CommandBase {
     private Drivetrain drivetrainSubsystem;
     // l
     private PIDController angleController;
     private double maxRotate;
 
+    private double angle;
+
     private double ff = 0.0; // retune
 
     /**
-     * Align robot with the target using the limelight
+     * Turn the robot a certain angle
      */
-    public RotateToAngle(Drivetrain drivetrainSubsystem, double angle) {
+    public RotateAngle(Drivetrain drivetrainSubsystem, double angle) {
         this.drivetrainSubsystem = drivetrainSubsystem;
         this.maxRotate = 0.5 * SwerveConstants.Swerve.maxAngularVelocity;
+        this.angle = RobotContainer.gyro.getGyroscopeRotation().getRadians() + angle;
 
-        angleController = new PIDController(2, 0, 0);
+        // if (this.angle > 180){
+        //     this.angle -= 360;
+        // }
+        // else if (this.angle < -180){
+        //     this.angle += 360;
+        // }
+
+        angleController = new PIDController(2, 0, 1);
         // TODO retune PID
         angleController.setTolerance(Units.degreesToRadians(3.5));
         angleController.enableContinuousInput(-Math.PI, Math.PI);
@@ -37,10 +47,9 @@ public class RotateToAngle extends CommandBase {
 
     @Override
     public void execute() {
-        double error = MathUtil.angleModulus(RobotContainer.gyro.getGyroscopeRotation().getRadians())
-                - drivetrainSubsystem.getRealZero().getRadians();
+        double error = MathUtil.angleModulus(RobotContainer.gyro.getGyroscopeRotation().getRadians()) - angle;
         double rotation = (MathUtil.clamp(angleController.calculate(error, 0.0) + (ff * Math.signum(-error)),
-                -maxRotate, maxRotate));
+                -maxRotate, maxRotate)); // change setpoint?
         drivetrainSubsystem.drive(new Translation2d(0.0, 0.0), rotation, true, true);
     }
 
